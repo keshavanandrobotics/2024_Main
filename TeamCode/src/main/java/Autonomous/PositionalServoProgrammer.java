610e095104febddd78e40b3f78c2bc5e1a691399
KeshavAnandCode <keshavmathguy@gmail.com>
2025-01-01 13:02:27 -0600

    1/1/2025

---
+++ TeamCode/src/main/java/Autonomous/PositionalServoProgrammer.java
@@ -0,0 +1,54 @@
+package Autonomous;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import Robot;
+
+@Config
+@TeleOp
+public class PositionalServoProgrammer extends LinearOpMode {
+
+    public static double TEST_POSITION = 0.501;
+    public static double MOVE_POSITION = 0.501;
+    public static double PIVOT_POSITION = 0.501;
+
+    public static double ROTATE_POSITION = 0.501;
+
+
+
+
+
+
+
+    Robot robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        robot = new Robot(hardwareMap);
+
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            if (TEST_POSITION != 0.501){robot.testServo.setPosition(TEST_POSITION);}
+            if (PIVOT_POSITION != 0.501){robot.clawPivot.setPosition(PIVOT_POSITION);}
+            if (MOVE_POSITION != 0.501){robot.clawMove.setPosition(MOVE_POSITION);}
+            if (ROTATE_POSITION != 0.501){robot.clawMove.setPosition(ROTATE_POSITION);}
+
+
+
+
+        }
+
+
+    }
+}
+
---
+++ TeamCode/src/main/java/Teleop/Singletons/DeadZones.java
@@ -0,0 +1,18 @@
+package Teleop.Singletons;
+
+import java.util.List;
+import java.util.Objects;
+
+public class DeadZones {
+
+    public static double Linear (double input, double threshold) {
+         if (Math.abs(input)>threshold) {
+             return input;
+         } else {
+             return 0;
+         }
+
+    }
+
+
+}
