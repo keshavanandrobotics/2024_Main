4e7033b66bb62d0b1cdf78c7eb2f217e10f2065d
KeshavAnandCode <keshavmathguy@gmail.com>
2025-01-01 22:41:56 -0600

    1/1/2025 - Folder

---
+++ TeamCode/src/main/java/teamcode/pedroPathing/constants/FConstants.java
@@ -0,0 +1,18 @@
+package teamcode.pedroPathing.constants;
+
+import com.pedropathing.localization.Localizers;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.util.CustomFilteredPIDFCoefficients;
+import com.pedropathing.util.CustomPIDFCoefficients;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class FConstants {
+    static {
+        // Select our localizer
+        FollowerConstants.localizers = Localizers.PINPOINT;
+
+        // We can change the value of any variable/constant of FollowerConstants.
+        FollowerConstants.mass = 10; // In kg
+        //TODO: Update the mass
+    }
+}
