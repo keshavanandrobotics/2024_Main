326e6205ec74fa97ecdfece0bc28385066c34eb7
DanTheMan-byte <danibarrf@gmail.com>
2025-06-13 14:44:55 -0500

    6/13 - VARS values

--- TeamCode/src/main/java/teamcode/Teleop/Singletons/VARS.java
+++ TeamCode/src/main/java/teamcode/Teleop/Singletons/VARS.java
@@ -16,53 +16,54 @@ public class VARS {
 
     public static double CLAW_LOOSE_GRAB = 0.74;
 
-    public static double ROTATE_NEUTRAL = 0.9;
+    public static double ROTATE_NEUTRAL = 0.98;
 
 
     public static double ROTATE_MTI_AUTON_SCORE = 0.28;
 
-    public static double ROTATE_AUTON_SPEC_SCORE = 0.75;
+    //public static double ROTATE_AUTON_SPEC_SCORE = 0.75;
 
 
 
-    public static double ROTATE_90 = 0.44;
-    public static double ROTATE_FLIP = 0;
+    public static double ROTATE_90 = 0.52;
+    public static double ROTATE_FLIP = 0.06;
     public static double ROTATE_LM3_SPECIMEN_AUTON = 0.23;
 
 
 
-    public static double MOVE_WALL_INTAKE = 0.9;
+    public static double MOVE_WALL_INTAKE = 0.07;
 
 
-    public static double MOVE_OUTTAKE = 0.9;
-    public static double MOVE_SPECIMEN_SCORE = 0.46;
-    public static double MOVE_ALL_OUT = 0.4;
-    public static double MOVE_HOVER_SAMPLE = 0.48;
+    public static double MOVE_OUTTAKE = 0.07;
+    //public static double MOVE_SPECIMEN_SCORE = 0.5;
+    //public static double MOVE_ALL_OUT = 0.5;
+    public static double MOVE_HOVER_SAMPLE = 0.44;
 
     public static double MOVE_AUTO_HOVER = 0.45;
 
-    public static double MOVE_RAISED = 0.7;
+    //public static double MOVE_RAISED = 0.25;
 
 
-    public static double MOVE_PICKUP_SAMPLE = 0.41;
-    public static double MOVE_AUTONOMOUS_INIT = 0.56;
+    public static double MOVE_PICKUP_SAMPLE = 0.47;
+    //public static double MOVE_AUTONOMOUS_INIT = 0.5;
 
     public static double MOVE_AUTON_DRAG = 0.3;
     public static double MOVE_AUTON_HOVER = 0.4;
 
 
 
-    public static double PIVOT_OUTTAKE = 0.48;
-    public static double PIVOT_WALL_INTAKE = 0.48;
+    public static double PIVOT_OUTTAKE = 0.45;
+    public static double PIVOT_WALL_INTAKE = 0.45;
 
     public static double PIVOT_AUTON_DRAG = 0.6;
     public static double PIVOT_AUTON_HOVER = 0.5;
 
-    public static double PIVOT_SPECIMEN_SCORE = 0.42;
-    public static double PIVOT_ALL_OUT = 0.59;
-    public static double PIVOT_SAMPLE_PICKUP = 0.96;
+    //public static double PIVOT_SPECIMEN_SCORE = 0.42;
+    //public static double PIVOT_ALL_OUT = 0.59;
+    public static double PIVOT_HOVER_SAMPLE = 0.895;
+    public static double PIVOT_SAMPLE_PICKUP = 0.87;
 
-    public static double PIVOT_AUTONOMOUS_INIT = 0.05;
+    //public static double PIVOT_AUTONOMOUS_INIT = 0.5;
     public static double LEFT_SPRING_OFF = 0.45;
     public static double LEFT_SPRING_ON = 0.2;
     public static double LEFT_SPRING_IN = 0.45;
@@ -72,18 +73,21 @@ public class VARS {
     public static double RIGHT_SPRING_IN = 0;
 
 
-    public static double LEFT_HOLD_OFF = 0.75;
-    public static double LEFT_HOLD_ON = 1;
+    public static double LEFT_HOLD_OFF = 0.22;
+    public static double LEFT_HOLD_ON = 0;
 
-    public static double RIGHT_HOLD_OFF = 0.25;
-    public static double RIGHT_HOLD_ON = 0;
-
-
+    public static double RIGHT_HOLD_OFF = 0.78;
+    public static double RIGHT_HOLD_ON = 1;
 
 
+    public static double LEFT_PTO_OFF = 0.28;
+    public static double LEFT_PTO_ON = 0.41;
+    public static double RIGHT_PTO_OFF = 0.9;
+    public static double RIGHT_PTO_ON = 0.78;
 
 
 
+    //TO BE DETERMINED
     public static int HIGH_SPECIMEN_POS = 22750/4;
 
     public static int HIGH_SPECIMEN_POS_AUTO_FIRST = 21100/4;
