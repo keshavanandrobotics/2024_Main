a40f8949af7e13fed0bbf66412aa138861316f42
DanTheMan-byte <danibarrf@gmail.com>
2025-06-13 15:13:33 -0500

    6/13 - servos

--- TeamCode/src/main/java/teamcode/Teleop/Singletons/VARS.java
+++ TeamCode/src/main/java/teamcode/Teleop/Singletons/VARS.java
@@ -21,7 +21,7 @@ public class VARS {
 
     public static double ROTATE_MTI_AUTON_SCORE = 0.28;
 
-    //public static double ROTATE_AUTON_SPEC_SCORE = 0.75;
+    public static double ROTATE_AUTON_SPEC_SCORE = 0.8;
 
 
 
@@ -35,17 +35,17 @@ public class VARS {
 
 
     public static double MOVE_OUTTAKE = 0.07;
-    //public static double MOVE_SPECIMEN_SCORE = 0.5;
-    //public static double MOVE_ALL_OUT = 0.5;
+    public static double MOVE_SPECIMEN_SCORE = 0.45;
+    public static double MOVE_ALL_OUT = 0.45;
     public static double MOVE_HOVER_SAMPLE = 0.44;
 
     public static double MOVE_AUTO_HOVER = 0.45;
 
-    //public static double MOVE_RAISED = 0.25;
+    public static double MOVE_RAISED = 0.1;
 
 
     public static double MOVE_PICKUP_SAMPLE = 0.47;
-    //public static double MOVE_AUTONOMOUS_INIT = 0.5;
+    public static double MOVE_AUTONOMOUS_INIT = 0.3;
 
     public static double MOVE_AUTON_DRAG = 0.3;
     public static double MOVE_AUTON_HOVER = 0.4;
@@ -58,12 +58,12 @@ public class VARS {
     public static double PIVOT_AUTON_DRAG = 0.6;
     public static double PIVOT_AUTON_HOVER = 0.5;
 
-    //public static double PIVOT_SPECIMEN_SCORE = 0.42;
-    //public static double PIVOT_ALL_OUT = 0.59;
+    public static double PIVOT_SPECIMEN_SCORE = 0.4;
+    public static double PIVOT_ALL_OUT = 0.6;
     public static double PIVOT_HOVER_SAMPLE = 0.895;
     public static double PIVOT_SAMPLE_PICKUP = 0.87;
 
-    //public static double PIVOT_AUTONOMOUS_INIT = 0.5;
+    public static double PIVOT_AUTONOMOUS_INIT = 0.3;
     public static double LEFT_SPRING_OFF = 0.45;
     public static double LEFT_SPRING_ON = 0.2;
     public static double LEFT_SPRING_IN = 0.45;
