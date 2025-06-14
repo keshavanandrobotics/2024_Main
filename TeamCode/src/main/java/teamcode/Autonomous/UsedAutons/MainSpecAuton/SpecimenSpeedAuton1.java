00bde81ea38ac2d541e121578a55c9d40eeb15d2
DanTheMan-byte <danibarrf@gmail.com>
2025-06-14 14:40:13 -0500

    6/14 - 6+0 #1

---
+++ TeamCode/src/main/java/teamcode/Autonomous/UsedAutons/MainSpecAuton/SpecimenSpeedAuton1.java
@@ -0,0 +1,503 @@
+package teamcode.Autonomous.UsedAutons.MainSpecAuton;
+
+import static teamcode.Autonomous.Disabled.Poses.AUTON_START_POSE;
+import static teamcode.Autonomous.UsedAutons.MainSpecAuton.SPEC_AUTO_VARS.*;
+
+import static teamcode.Teleop.Singletons.VARS.*;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.limelightvision.LLResult;
+
+import teamcode.Autonomous.RoadRunner.PinpointDrive;
+import teamcode.Robot;
+
+@Config
+@Autonomous (preselectTeleOp = "Drive_V4")
+public class SpecimenSpeedAuton1 extends LinearOpMode {
+
+    public double angle = 0.0;
+    Robot robot;
+
+
+
+
+
+    public TranslationalVelConstraint VEL_CONSTRAINT = new TranslationalVelConstraint(MAX_PUSHING_VEL);
+    public ProfileAccelConstraint ACCEL_CONSTRAINT = new ProfileAccelConstraint(-Math.abs(MAX_PUSHING_DECCEL), MAX_PUSHING_ACCEL);
+
+    public TranslationalVelConstraint VEL_CONSTRAINT2 = new TranslationalVelConstraint(MAX_CYCLING_VEL);
+    public ProfileAccelConstraint ACCEL_CONSTRAINT2 = new ProfileAccelConstraint(-Math.abs(MAX_CYCLING_DECCEL), MAX_CYCLING_ACCEL);
+
+    public Action LimelightRotate(){
+
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                LLResult result = robot.limelight.getLatestResult();
+
+
+                double[] pythonOutputs = result.getPythonOutput();
+
+                if (pythonOutputs != null && pythonOutputs.length > 0) {
+                    angle = pythonOutputs[3];
+
+                    if (angle > 90) {
+                        angle -= 180;
+                    }
+
+                    telemetry.addData("ANGLE", angle);
+                }
+
+                return false;
+            }
+        };
+    }
+
+    public Action ExtendoPID(double power){
+
+        return new Action() {
+
+
+
+            final double finalPower = power;
+
+
+
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
+                robot.extendo.setPower(finalPower);
+                return false;
+            }
+
+        };
+    }
+
+
+
+
+
+    public Action Servos(double clawPos, double rotatePos, double movePos, double pivotPos){
+
+        return new Action() {
+
+            boolean claw = true;
+            boolean rotate = true;
+            boolean move = true;
+            boolean pivot = true;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
+                if (clawPos == 0.501){
+                    claw = false;
+                } else {
+
+
+                    robot.claw.setPosition(clawPos);
+
+
+                }
+                if (rotatePos == 0.501){
+                    rotate = false;
+                } else {
+
+
+
+
+                    robot.clawRotate.setPosition(rotatePos);
+                }
+                if (movePos== 0.501){
+                    move = false;
+                } else {
+
+
+
+
+                    robot.clawLeftMove.setPosition(movePos);
+                    robot.clawRightMove.setPosition(1-movePos);
+                }
+                if (pivotPos== 0.501){
+                    pivot = false;
+                } else {
+
+                    robot.clawPivot.setPosition(pivotPos);
+                }
+
+
+
+
+                return false;
+
+
+
+
+            }
+        };
+
+
+    }
+
+    public Action LinearSlidePID(int position, double holdPower){
+
+        return new Action() {
+
+            private final PIDController controller = new PIDController(0.0006,0,0.00001);
+
+
+
+
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
+                double linearSlidePosition = -robot.linearSlideEncoder.getCurrentPosition();
+
+                controller.setPID(0.0006,0,0.00001);
+
+                double power = controller.calculate(linearSlidePosition, position) + 0.08;
+
+                robot.leftSlide.setPower(power);
+                robot.rightSlide.setPower(power);
+
+                telemetry.addData("power", power);
+                telemetry.addData("Target", position);
+                telemetry.addData("pos", linearSlidePosition);
+
+                if (Math.abs(position - linearSlidePosition)<475){
+                    telemetry.addLine("Success");
+                    telemetry.update();
+
+
+                    robot.leftSlide.setPower(holdPower);
+                    robot.rightSlide.setPower(holdPower);
+
+
+                    return false;
+
+                } else {
+
+
+                    telemetry.update();
+
+                    return  true;
+
+                }
+
+            }
+
+        };
+    }
+
+    public Action Wait (double time){
+        return new Action() {
+
+            int ticker = 1;
+
+            double stamp = 0.0;
+
+            final double desiredTime = time;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
+                if (ticker ==1){
+                    stamp = getRuntime();
+                }
+
+                ticker ++;
+
+                double runTime = getRuntime() - stamp;
+
+                return (runTime < desiredTime);
+
+
+            }
+
+        };
+    }
+
+
+
+
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //Initialization:
+
+        robot = new Robot(hardwareMap);
+
+
+        robot.drive = new PinpointDrive(hardwareMap, AUTON_START_POSE);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        //Motor Inits:
+
+        robot.linearSlideEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.linearSlideEncoder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        robot.extendo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.extendo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        robot.claw.setPosition(CLAW_CLOSED);
+        robot.clawLeftMove.setPosition(MOVE_AUTONOMOUS_INIT);
+        robot.clawRightMove.setPosition(1-MOVE_AUTONOMOUS_INIT);
+        robot.clawPivot.setPosition(PIVOT_AUTONOMOUS_INIT);
+        robot.clawRotate.setPosition(ROTATE_FLIP);
+
+        robot.rightStabilizer.setPosition(RIGHT_HOLD_OFF);
+        robot.leftStabilizer.setPosition(LEFT_HOLD_OFF);
+        robot.leftPTO.setPosition(LEFT_PTO_OFF);
+        robot.rightPTO.setPosition(RIGHT_PTO_OFF);
+
+        //Trajectories
+
+
+        TrajectoryActionBuilder firstScore = robot.drive.actionBuilder(AUTON_START_POSE)
+                .strafeToLinearHeading(new Vector2d(FIRST_SPEC_SCORE_X, FIRST_SPEC_SCORE_Y), 0, VEL_CONSTRAINT2, ACCEL_CONSTRAINT2);
+
+        TrajectoryActionBuilder subPickup = robot.drive.actionBuilder(new Pose2d(FIRST_SPEC_SCORE_X, FIRST_SPEC_SCORE_Y, 0))
+                .strafeToLinearHeading(new Vector2d(SUB_PICKUP_X1, SUB_PICKUP_Y1), 0, VEL_CONSTRAINT2, ACCEL_CONSTRAINT2);
+
+        TrajectoryActionBuilder subDrop = robot.drive.actionBuilder(new Pose2d(SUB_PICKUP_X1, SUB_PICKUP_Y1, 0))
+                .strafeToLinearHeading(new Vector2d(DROP_X, DROP_Y), Math.toRadians(DROP_HEADING));
+
+        TrajectoryActionBuilder firstWallGrab = robot.drive.actionBuilder(new Pose2d(X10, Y10, 0))
+                .strafeToLinearHeading(new Vector2d(FIRST_WALL_GRAB_X, FIRST_WALL_GRAB_Y), 0, VEL_CONSTRAINT2,ACCEL_CONSTRAINT2);
+
+        TrajectoryActionBuilder secondScore = robot.drive.actionBuilder(new Pose2d(FIRST_WALL_GRAB_X, FIRST_WALL_GRAB_Y, 0))
+                .strafeToLinearHeading(new Vector2d(SPEC_SCORE_X,SPEC_SCORE_Y), Math.toRadians(SPEC_SCORE_HEADING),VEL_CONSTRAINT2, ACCEL_CONSTRAINT2);
+
+        TrajectoryActionBuilder subsequentWallGrabs = robot.drive.actionBuilder(new Pose2d(SPEC_SCORE_X, SPEC_SCORE_Y, Math.toRadians(SPEC_SCORE_HEADING)))
+                .strafeToLinearHeading(new Vector2d(WALL_GRAB_X,WALL_GRAB_Y), Math.toRadians(0),VEL_CONSTRAINT2, ACCEL_CONSTRAINT2);
+
+        TrajectoryActionBuilder subsequentScores = robot.drive.actionBuilder(new Pose2d(WALL_GRAB_X,WALL_GRAB_Y,0))
+                .strafeToLinearHeading(new Vector2d(SPEC_SCORE_X,SPEC_SCORE_Y), Math.toRadians(SPEC_SCORE_HEADING),VEL_CONSTRAINT2, ACCEL_CONSTRAINT2);
+
+
+        robot.drive.rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.drive.rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        robot.drive.leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        robot.drive.leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        robot.rightSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        while(opModeInInit()){
+
+
+            telemetry.addData("AUTON IS RED", AUTON_RED);
+
+            telemetry.addData("TAKE YELLOWS", !SPEC_MODE);
+
+            if (gamepad1.b){
+                AUTON_RED = true;
+            }
+
+            if (gamepad1.x){
+                AUTON_RED = false;
+            }
+
+            if (gamepad1.a){
+                SPEC_MODE = true;
+            }
+
+            if (gamepad1.y){
+                SPEC_MODE = false;
+            }
+
+            telemetry.update();
+        }
+
+
+
+        if (isStopRequested()) return;
+
+        if (opModeIsActive()){
+
+
+
+            Actions.runBlocking(
+                    new ParallelAction(
+                            firstScore.build(),
+                            LinearSlidePID(HIGH_SPECIMEN_POS_AUTO_FIRST, 0),
+                            Servos(CLAW_CLOSED, ROTATE_NEUTRAL, MOVE_SPECIMEN_SCORE, PIVOT_SPECIMEN_SCORE),
+                            new SequentialAction(
+                                    Wait(FIRST_SCORE_WAIT),
+                                    ExtendoPID(1)
+                            )
+                    )
+            );
+
+            Actions.runBlocking(
+                    new ParallelAction(
+                            ExtendoPID(-1),
+                            LinearSlidePID(LINEAR_SLIDE_LOWER_THRESHOLD, 0)
+                    )
+            );
+
+            Actions.runBlocking(
+                    new ParallelAction(
+                            Servos(CLAW_OPEN, ROTATE_90, MOVE_HOVER_SAMPLE, MOVE_PICKUP_SAMPLE),
+                            ExtendoPID(1)
+                    )
+            );
+
+
+            Actions.runBlocking(
+                    new SequentialAction(
+                            firstWallGrab.build(),
+                            Wait(HUMAN_PLAYER_WAIT),
+                            Servos(CLAW_CLOSED, 0.501, 0.501, .501),
+                            Wait(CLAW_CLOSE_TIME)
+                    )
+            );
+
+
+
+            Actions.runBlocking(
+                    new ParallelAction(
+                            secondScore.build(),
+                            Servos(0.501, ROTATE_AUTON_SPEC_SCORE, MOVE_SPECIMEN_SCORE, PIVOT_SPECIMEN_SCORE),
+                            LinearSlidePID(HIGH_SPECIMEN_POS_AUTO_FIRST, 0.12),
+                            new SequentialAction(
+                                    Wait(EXTENDO_OUT_WAIT),
+                                    ExtendoPID(1)
+                            )
+                    )
+            );
+
+            for (int i = 0; i<4; i++) {
+
+                Actions.runBlocking(
+                        new SequentialAction(
+                                Servos(CLAW_OPEN, 0.501, 0.501, 0.501),
+                                Wait(CLAW_OPEN_TIME),
+                                new ParallelAction(
+                                        subsequentWallGrabs.build(),
+                                        new SequentialAction(
+                                                ExtendoPID(-1),
+                                                Wait(EXTENDO_RETRACT_WAIT),
+                                                new ParallelAction(
+                                                        LinearSlidePID(LINEAR_SLIDE_LOWER_THRESHOLD, -0.12),
+                                                        Servos(CLAW_OPEN, ROTATE_FLIP, MOVE_WALL_INTAKE, PIVOT_WALL_INTAKE),
+                                                        ExtendoPID(-0.5)
+
+                                                )
+                                        )
+                                )
+
+                        )
+
+
+                );
+
+                //Actions.runBlocking(
+                //        new SequentialAction(
+                //                Wait(HUMAN_PLAYER_WAIT),
+                //                ExtendoPID(EXTENDO_GRAB_THRESHOLD, 1, 1),
+                //                Wait(EXTENDO_IN_WAIT),
+                //                Servos(CLAW_CLOSED, 0.501, 0.501, .501),
+                //                Wait(CLAW_CLOSE_TIME)
+                //        )
+                //);
+
+                //while (!limitClawChecker()){
+                //    Actions.runBlocking(
+                //            new ParallelAction(
+                //                    ExtendoPID(EXTENDO_GRAB_THRESHOLD, 0, 0),
+                //                    Servos(CLAW_OPEN, 0.501,0.501,0.501),
+                //                    new SequentialAction(
+                //                            Wait(CLAW_OPEN_TIME),
+                //                            retrySpec.build(),
+                //                            Wait(HUMAN_PLAYER_WAIT),
+                //                            ExtendoPID(EXTENDO_GRAB_THRESHOLD, 1, 1),
+                //                            Wait(EXTENDO_IN_WAIT),
+                //                            Servos(CLAW_CLOSED, 0.501, 0.501, .501),
+                //                            Wait(CLAW_CLOSE_TIME)
+                //                    )
+                //            )
+                //    );
+                //}
+
+                Actions.runBlocking(
+                        new SequentialAction(
+                                Wait(HUMAN_PLAYER_WAIT),
+                                ExtendoPID(-1),
+                                Wait(EXTENDO_IN_WAIT),
+                                Servos(CLAW_CLOSED, 0.501, 0.501, .501),
+                                Wait(CLAW_CLOSE_TIME),
+                                new ParallelAction(
+                                        subsequentScores.build(),
+                                        Servos(0.501, ROTATE_AUTON_SPEC_SCORE, MOVE_SPECIMEN_SCORE, PIVOT_SPECIMEN_SCORE),
+                                        LinearSlidePID(HIGH_SPECIMEN_POS, 0.12),
+                                        new SequentialAction(
+                                                Wait(EXTENDO_OUT_WAIT),
+                                                ExtendoPID(1)
+                                        )
+
+                                )
+                        )
+                );
+            }
+
+
+            Actions.runBlocking(
+                    new SequentialAction(
+                            Servos(CLAW_OPEN, 0.501, 0.501, 0.501),
+                            Wait(CLAW_OPEN_TIME),
+                            new ParallelAction(
+                                    subsequentWallGrabs.build(),
+                                    new SequentialAction(
+                                            ExtendoPID(-1),
+                                            Wait(EXTENDO_RETRACT_WAIT),
+                                            new ParallelAction(
+                                                    LinearSlidePID(LINEAR_SLIDE_LOWER_THRESHOLD, -0.12),
+                                                    Servos(CLAW_OPEN, ROTATE_FLIP, MOVE_WALL_INTAKE, PIVOT_WALL_INTAKE),
+                                                    ExtendoPID(0)
+                                            )
+                                    )
+                            )
+
+                    )
+
+
+            );
+
+            sleep(10000);
+
+
+
+
+        }
+
+
+    }
+
+
+}
