2e3f51b4c517f8f4ed74b020f634f9785c14ed0e
DanTheMan-byte <danibarrf@gmail.com>
2025-06-17 12:16:06 -0500

    6/16 - 5+1 SPEC_AUTO_VARS comments

--- TeamCode/src/main/java/teamcode/Autonomous/UsedAutons/MainSpecAuton/SPEC_AUTO_VARS.java
+++ TeamCode/src/main/java/teamcode/Autonomous/UsedAutons/MainSpecAuton/SPEC_AUTO_VARS.java
@@ -8,7 +8,7 @@ import com.acmerobotics.dashboard.config.Config;
 public class SPEC_AUTO_VARS {
 
 
-    //PUSHING WAYPOINTS - CHRONOLOGICAL
+    //PUSHING WAYPOINTS - CHRONOLOGICAL - DO NOT EDIT ANY OF THESE
 
     public static double X1 = 15, Y1 = -30; //INITIAL DIAGONAL STRAFE
     public static double X2 = 55, Y2 = -26; //GOING PAST THE FIRST SPEC
@@ -33,27 +33,27 @@ public class SPEC_AUTO_VARS {
 
     //Cycling Coordinates
 
-    public static double FIRST_WALL_GRAB_X = -1, FIRST_WALL_GRAB_Y = -22; // SPOT FOR HUMAN PLAYER GRAB FIRST SPEC
-    public static double WALL_GRAB_X = 1, WALL_GRAB_Y = -22; //SPOT FOR HUMAN PLAYER GRAB
+    public static double FIRST_WALL_GRAB_X = -1, FIRST_WALL_GRAB_Y = -22; // SPOT FOR HUMAN PLAYER GRAB FIRST SPEC - DO NOT EDIT
+    public static double WALL_GRAB_X = 1, WALL_GRAB_Y = -22; //SPOT FOR HUMAN PLAYER GRAB - DO NOT EDIT
 
     public static double FIRST_SPEC_SCORE_X = 20, FIRST_SPEC_SCORE_Y = 10; // SPOT FOR PRELOAD SCORE
 
-    public static double SPEC_SCORE_X = 25, SPEC_SCORE_Y = 8, SPEC_SCORE_HEADING = 30; //SPOT FOR SCORING
+    public static double SPEC_SCORE_X = 25, SPEC_SCORE_Y = 8, SPEC_SCORE_HEADING = 30; //SPOT FOR SCORING - DO NOT EDIT
 
     public static double SPEC_SCORE_X2 = 25, SPEC_SCORE_Y2 = 9, SPEC_SCORE_HEADING2 = 20;
-    public static double RETRY_SPEC_X = 15, RETRY_SPEC_Y = -22; // SPOT TO MOVE TO IF FAILED SPEC
+    public static double RETRY_SPEC_X = 15, RETRY_SPEC_Y = -22; // SPOT TO MOVE TO IF FAILED SPEC - DO NOT EDIT
 
-    // Pickup up coordinates
-    public static double PICKUP_X1 = 18, PICKUP_Y1 = -10;
+    // Pickup up coordinates - EDIT THESE
+    public static double PICKUP_X1 = 16, PICKUP_Y1 = -10;
     public static double PICKUP_X2 = 14, PICKUP_Y2 = -22;
     public static double PICKUP_X3 = 14, PICKUP_Y3 = -34;
     public static double PICKUP_HEADING = -45;
-    public static double DROP_X1 = 18, DROP_Y1 = -8;
-    public static double DROP_X2 = 18, DROP_Y2 = -16;
-    public static double DROP_X3 = 18, DROP_Y3 = -24;
+    public static double DROP_X1 = 15, DROP_Y1 = -8;
+    public static double DROP_X2 = 15, DROP_Y2 = -16;
+    public static double DROP_X3 = 15, DROP_Y3 = -24;
     public static double DROP_HEADING = -135;
 
-    //sample score
+    //sample score - EDIT THESE
     public static double SAMPLE_SCORE_X1 = 5, SAMPLE_SCORE_Y1 = 68, SAMPLE_SCORE_TRAVELHEADING = -90;
     public static double SAMPLE_SCORE_X2 = 5, SAMPLE_SCORE_Y2 = 72, SAMPLE_SCORE_HEADING = -45;
 
@@ -61,14 +61,14 @@ public class SPEC_AUTO_VARS {
 
 
 
-    //Trajectory Speeds - SHOULD NOT NEED EDITING
+    //Trajectory Speeds - SHOULD NOT NEED EDITING - DO NOT EDIT
 
     //SPEEDS FOR THE PUSHING PATHS
     public static double MAX_PUSHING_VEL = 140;
     public static double MAX_PUSHING_ACCEL = 140;
     public static double MAX_PUSHING_DECCEL = 80; //Only input positive number
 
-    //SPEEDS FOR THE SCORING AND CYCLING PATHS
+    //SPEEDS FOR THE SCORING AND CYCLING PATHS - DO NOT EDIT
 
     public static double MAX_CYCLING_VEL = 140;
     public static double MAX_CYCLING_ACCEL = 140;
@@ -80,24 +80,24 @@ public class SPEC_AUTO_VARS {
     public static double MAX_SAMPLE_DECCEL = 100;
 
     //WAITS
-    public static double PUSHING_SERVO_ROTATE = 1; //SHOULD NOT NEED EDITING, TIME BEFORE SERVO ROTATES WHILE PUSHING
-    public static double PUSHING_SERVO_OPEN = 1; //SHOULD NOT NEED EDITING, TIME BTWN ROTATION AND OPENING
-    public static double PUSHING_SERVO_PIVOT = 1; //SHOULD NOT NEED EDITING, TIME BTWN OPENING AND PIVOT MOVING
-    public static double PUSHING_SERVO_MOVE = 1; //SHOULD NOT NEED EDITING, TIME BTWN PIVOT MOVING AND MOVE MOVING
+    public static double PUSHING_SERVO_ROTATE = 1; // DO NOT EDIT, TIME BEFORE SERVO ROTATES WHILE PUSHING
+    public static double PUSHING_SERVO_OPEN = 1; // DO NOT EDIT, TIME BTWN ROTATION AND OPENING
+    public static double PUSHING_SERVO_PIVOT = 1; // DO NOT EDIT, TIME BTWN OPENING AND PIVOT MOVING
+    public static double PUSHING_SERVO_MOVE = 1; // DO NOT EDIT, TIME BTWN PIVOT MOVING AND MOVE MOVING
 
-    public static double HUMAN_PLAYER_WAIT = 0.0; //TIME ROBOT RESTS BEFORE EXTENDO IN TO GRAB SPECIMEN
+    public static double HUMAN_PLAYER_WAIT = 0.0; //TIME ROBOT RESTS BEFORE EXTENDO IN TO GRAB SPECIMEN - DO NOT EDIT
 
-    public static double CLAW_CLOSE_TIME = 0.25; //SHOULD NOT NEED EDITING, TIME THAT CLAW CLOSES FOR WALL GRABS
-    public static double CLAW_OPEN_TIME = 0.1; //TIME THAT CLAW OPENS FOR WHEN SCORING SPEC
+    public static double CLAW_CLOSE_TIME = 0.25; //SHOULD NOT NEED EDITING, TIME THAT CLAW CLOSES FOR WALL GRABS - DO NOT EDIT
+    public static double CLAW_OPEN_TIME = 0.1; //TIME THAT CLAW OPENS FOR WHEN SCORING SPEC - DO NOT EDIT
     public static double CLAW_DOWN_TIME = 0.1;
     public static double SAMPLE_DOWN_TIME = 0.5;
 
-    public static double EXTENDO_OUT_WAIT = 1; //WAIT TIME FOR EXTENDO TO GO OUT WHILE SCORING
+    public static double EXTENDO_OUT_WAIT = 1; // WAIT TIME FOR EXTENDO TO GO OUT WHILE SCORING - DO NOT EDIT
     public static double EXTENDO_ALL_OUT = 0.5;
     public static double EXTENDO_PICKUP_WAIT = 1;
     public static double EXTENDO_FIRST_WAIT = 0.2;
 
-    public static double EXTENDO_IN_WAIT = 0.1;
+    public static double EXTENDO_IN_WAIT = 0.1; // DO NOT EDIT
     public static double MOVE_WAIT = 1;
 
     public static double LEAVE_OBS = 1.0;
@@ -113,11 +113,11 @@ public class SPEC_AUTO_VARS {
     public static int EXTENDO_SCORE_THRESHOLD = 5000; //SHOULD NOT NEED EDITING, POSITIONS FOR EXTENDO OUT WHERE ACTION ENDS
     public static int EXTENDO_CYCLE_HUMAN_PLAYER = 3000; //POSITION BEFORE EXTENDO GOES IN AND GRABS 2-5 SPEC
 
-    public static double EXTENDO_RETRACT_WAIT = 0.6;
+    public static double EXTENDO_RETRACT_WAIT = 0.6; // DO NOT EDIT
 
     public static double FIRST_SCORE_WAIT = 0.8;
 
-    public static int LINEAR_SLIDE_LOWER_THRESHOLD = 500; //SHOULD NOT NEED EDITING, POSITION WHERE LINEAR SLIDE LOWERING ACTION ENDS
+    public static int LINEAR_SLIDE_LOWER_THRESHOLD = 1000; // DO NOT EDIT, POSITION WHERE LINEAR SLIDE LOWERING ACTION ENDS
 
 
 
